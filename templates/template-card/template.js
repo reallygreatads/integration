f48dcd926f66f7d7bc78819809f0481e4c6613ad
Jamie Bradley <jamie@stackupdigital.co.uk>
2024-09-05 12:38:08 +0100

    Full end to end fetch request

--- templates/template-card/template.js
+++ templates/template-card/template.js
@@ -2,48 +2,48 @@ export const renderTemplateCard = ({ adverts }) => {
   if (adverts.length === 0) {
     return `
       <div id="rga_ad-wrapper">
-        <div class="rga_simple-ad-wrap"></div>
+        <div class="rga_card-ad-wrap"></div>
       </div>
     `;
   }
 
   return `
     <div id="rga_ad-wrapper">
-      <div class="rga_simple-ad-wrap">
+      <div class="rga_card-ad-wrap">
 
         ${adverts
-          .map((advert) => {
+          .map((advert, index) => {
             return `
-            <article class="rga_simple-ad-template">
-              <h2 class="rga_simple-ad-template__title rga_simple-ad-template__title--mobile">
+            <article class="rga_card-ad-template ${
+              adverts?.length === 2 && index === 0
+                ? "rga_card-ad-template--offset-2"
+                : adverts?.length === 1 && index === 0
+                ? "rga_card-ad-template--offset-3"
+                : ""
+            }">
+              <h2 class="rga_card-ad-template__title">
                 ${advert.title}
               </h2>
 
-              <div class="rga_simple-ad-template__image-wrap">
+              <div class="rga_card-ad-template__image-wrap">
                 ${
                   advert.image_url &&
                   `
                   <img
                     src="${advert.image_url}"
                     alt=""
-                    class="rga_simple-ad-template__image"
+                    class="rga_card-ad-template__image"
                   />
                 `
                 }
               </div>
 
-              <div class="rga_simple-ad-template__content">
-                <h3
-                  class="rga_simple-ad-template__title rga_simple-ad-template__title--desktop"
-                >
-                  ${advert.title}
-                </h3>
-
-                <div class="rga_simple-ad-template__body">
+              <div class="rga_card-ad-template__content">
+                <div class="rga_card-ad-template__body">
                   ${
                     advert.description?.length > 1
                       ? `
-                      <ul class="rga_simple-ad-template__list">
+                      <ul class="rga_card-ad-template__list">
                         ${advert.description
                           .map((item) => `<li>${item}</li>`)
                           .join("")}
@@ -54,9 +54,9 @@ export const renderTemplateCard = ({ adverts }) => {
                 </div>
               </div>
 
-              <a href="${
-                advert.click_url
-              }" class="rga_simple-ad-template__cta">${advert.cta_text}</a>
+              <a href="${advert.click_url}" class="rga_card-ad-template__cta">${
+              advert.cta_text
+            }</a>
             </article>
           `;
           })
