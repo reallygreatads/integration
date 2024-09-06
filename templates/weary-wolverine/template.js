bea0cc459a53012244ed44bdc203a8852f043dcd
Jamie Bradley <jamie@stackupdigital.co.uk>
2024-09-06 18:47:13 +0100

    Safari Fixes
    Applied a series of fixes to the ads in safari

--- templates/weary-wolverine/template.js
+++ templates/weary-wolverine/template.js
@@ -15,48 +15,50 @@ export const renderSimpleTemplate = ({ adverts }) => {
           .map((advert) => {
             return `
             <article class="rga_simple-ad-template">
-              <h2 class="rga_simple-ad-template__title rga_simple-ad-template__title--mobile">
-                ${advert.title}
-              </h2>
-
-              <div class="rga_simple-ad-template__image-wrap">
-                ${
-                  advert.image_url &&
-                  `
-                  <img
-                    src="${advert.image_url}"
-                    alt=""
-                    class="rga_simple-ad-template__image"
-                  />
-                `
-                }
-              </div>
-
-              <div class="rga_simple-ad-template__content">
-                <h3
-                  class="rga_simple-ad-template__title rga_simple-ad-template__title--desktop"
-                >
+              <div class="rga_simple-ad-template__grid">
+                <h2 class="rga_simple-ad-template__title rga_simple-ad-template__title--mobile">
                   ${advert.title}
-                </h3>
+                </h2>
 
-                <div class="rga_simple-ad-template__body">
+                <div class="rga_simple-ad-template__image-wrap">
                   ${
-                    advert.description?.length > 1
-                      ? `
-                      <ul class="rga_simple-ad-template__list">
-                        ${advert.description
-                          .map((item) => `<li>${item}</li>`)
-                          .join("")}
-                      </ul>
+                    advert.image_url &&
                     `
-                      : advert.description[0]
+                    <img
+                      src="${advert.image_url}"
+                      alt=""
+                      class="rga_simple-ad-template__image"
+                    />
+                  `
                   }
                 </div>
-              </div>
 
-              <a href="${
-                advert.click_url
-              }" class="rga_simple-ad-template__cta">${advert.cta_text}</a>
+                <div class="rga_simple-ad-template__content">
+                  <h3
+                    class="rga_simple-ad-template__title rga_simple-ad-template__title--desktop"
+                  >
+                    ${advert.title}
+                  </h3>
+
+                  <div class="rga_simple-ad-template__body">
+                    ${
+                      advert.description?.length > 1
+                        ? `
+                        <ul class="rga_simple-ad-template__list">
+                          ${advert.description
+                            .map((item) => `<li>${item}</li>`)
+                            .join("")}
+                        </ul>
+                      `
+                        : advert.description[0]
+                    }
+                  </div>
+                </div>
+
+                <a href="${
+                  advert.click_url
+                }" class="rga_simple-ad-template__cta">${advert.cta_text}</a>
+              </div>
             </article>
           `;
           })
